d3dc172a16cb7f49a408e4e6c68973eb49b746d8
DeanSami <deanab@ac.sce.ac.il>
2018-12-03 00:01:35 +0200

    minor change for editor adding global event

--- calServ/controllers/EditorController.js
+++ calServ/controllers/EditorController.js
@@ -46,13 +46,13 @@ module.exports = (app) => {
         let user = await User.findOne({ username: req.body.username, token: req.body.token, permission: 'editor' });
         if (user) {
             let newGlobalEvent = new GlobalEvent({
-                title: req.body.title,
-                start: req.body.start,
-                end: req.body.end,
-                description: req.body.description,
+                title: req.body.event.title,
+                start: req.body.event.start,
+                end: req.body.event.end,
+                description: req.body.event.description,
                 postedBy: user.username,
-                category: req.body.category,
-                place: req.body.place
+                category: req.body.event.category,
+                place: req.body.event.place
             });
             let promise = await newGlobalEvent.save();
             if (promise) {
